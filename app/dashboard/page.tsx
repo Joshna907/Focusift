7b66744214c9f917d7233c6eaaec4e0707d4ed7c
Joshna Waikar <joshnawaikar@gmail.com>
2025-07-19 07:31:27 -0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
-
+import { authOptions } from '@/lib/auth';
 export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
 
